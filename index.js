9277ea66234d358d3fc68705936bb5e3709edf71
Falih Muqsith Setiyoko <45122509+FalihMS@users.noreply.github.com>
2022-02-17 12:12:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.set('port', '3000')
+app.set('port', process.env.PORT || 3000)
 
 const server = http.createServer(app)
 const io = new Server(server);
